1df5e9acb6bbc43f09a67104816f79c73cbb338b
otokay <otokay@infina.com.tr>
2024-09-27 09:06:23 +0300

    Add Day27Algo1.java to the september2024 directory

---
+++ september2024/Day27Algo1.java
@@ -0,0 +1,8 @@
+package september2024;
+
+//Klayveden girilen saat(0-11) ve dakika(0-59) değerlerine göre akrep ile yelkovan arasındaki açıyı hesaplayan program
+public class Day27Algo1 {
+    public static void main(String[] args) {
+
+    }
+}
