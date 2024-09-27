c96cb672179af77801225a7141ff8276b2467633
otokay <otokay@infina.com.tr>
2024-09-27 09:06:18 +0300

    Rename Day2Algo2.java to Day26Algo2.java in the september2024 directory

--- september2024/Day26Algo2.java
+++ september2024/Day26Algo2.java
@@ -5,7 +5,7 @@ package september2024;
 // 1- 100'ün katı olan yıllardan sadece 400'e tam bölünebilenler artık yıldır.
 // 2- 4000, 16000, 24000 vb. yıllar, 400'e tam bölünebilmesine rağmen artık yıl değildir.
 // Bu durumda yıl<4000, olan yıllar için artık yıl hesaplama işlemi şu şekilde yapılabilir
-class Day2Algo2 {
+class Day26Algo2 {
     public static void main(String[] args) {
         int yil = 2025;
         if (yil > 4000) {
