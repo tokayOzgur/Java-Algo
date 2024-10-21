783e8e11b1e4df7e476f8a5b15cfb968aa968945
Özgür Tokay <ozytky@gmail.com>
2024-10-22 01:05:28 +0300

    Day9

---
+++ october2024/Day9.java
@@ -0,0 +1,16 @@
+package october2024;
+
+// girilen iki sayıdan en küçük ve en büyüğünü bulan program
+public class Day9 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 14;
+
+        if (number1 > number2) {
+            System.out.println("number1 > number2");
+        } else {
+            System.out.println("number2 > number1");
+        }
+
+    }
+}
