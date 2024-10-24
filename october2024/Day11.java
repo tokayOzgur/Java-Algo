b59f1daf90d8ea721cc1869d0ed6766d29b6baa2
Özgür Tokay <ozytky@gmail.com>
2024-10-25 00:35:21 +0300

    Refactor code to check if two numbers are multiples of each other

--- october2024/Day11.java
+++ october2024/Day11.java
@@ -2,5 +2,18 @@ package october2024;
 
 // girilen iki sayı birbirinin katı mı değilmi bulan program
 public class Day11 {
-    
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 5;
+
+        if (num1 % num2 == 0) {
+            System.out.println(num1 + " sayısı " + num2 + " sayısının katıdır.");
+        } else if (num2 % num1 == 0) {
+            System.out.println(num2 + " sayısı " + num1 + " sayısının katıdır.");
+        } else {
+            System.out.println("Sayılardan hiçbiri diğerinin katı değildir.");
+        }
+
+    }
+
 }
