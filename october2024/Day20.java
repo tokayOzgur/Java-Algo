7cf73e61a2434be423ab4e6ec533ebacb348a8bc
Özgür Tokay <ozytky@gmail.com>
2024-10-25 12:21:06 +0300

    day20

--- october2024/Day20.java
+++ october2024/Day20.java
@@ -2,5 +2,8 @@ package october2024;
 
 // girilen sayının, hangi sayının karekökü olduğunu bulan program
 public class Day20 {
-    
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println(num * num);
+    }
 }
