059540b97e936fa7f269db7f408c1caa4dd63b58
Özgür Tokay <ozytky@gmail.com>
2024-10-25 11:39:06 +0300

    day12

--- october2024/Day12.java
+++ october2024/Day12.java
@@ -2,5 +2,11 @@ package october2024;
 
 // 1-100 arasındaki sayıların toplamını bulan program
 public class Day12 {
-    
+    public static void main(String[] args) {
+        int toplam = 0;
+        for (int i = 1; i <= 100; i++) {
+            toplam += i;
+        }
+        System.out.println("1-100 arasındaki sayıların toplamı: " + toplam);
+    }
 }
