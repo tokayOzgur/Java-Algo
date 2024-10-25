43cce7ddb9c1c1784604bd7209fca0591ddf6f60
Özgür Tokay <ozytky@gmail.com>
2024-10-25 11:39:11 +0300

    day13

--- october2024/Day13.java
+++ october2024/Day13.java
@@ -2,5 +2,19 @@ package october2024;
 
 // girilen iki sayı arasındaki her bir sayının  karesini bulan program
 public class Day13 {
-    
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 5;
+        int minNum = 0;
+        if (num1 < num2)
+            minNum = num1;
+        else
+            minNum = num2;
+
+        for (int i = minNum; i <= num1 + num2 - minNum; i++) {
+            System.out.println(i + " sayısının karesi: " + i * i);
+        }
+
+    }
+
 }
