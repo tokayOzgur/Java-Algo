c8c13e6b8f232226940bff34c50ece7958525f30
Özgür Tokay <ozytky@gmail.com>
2024-10-25 11:39:27 +0300

    day15

--- october2024/Day15.java
+++ october2024/Day15.java
@@ -2,5 +2,28 @@ package october2024;
 
 // Girilen iki sayı arasındaki, her sayının faktoriyelini hesaplayan program
 public class Day15 {
-    
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 6;
+
+        int minNum, maxNum;
+        if (num1 > num2) {
+            minNum = num2;
+            maxNum = num1;
+        } else {
+            minNum = num1;
+            maxNum = num2;
+        }
+
+        for (int i = minNum; i < maxNum; i++) {
+            System.out.println(i + "sayısının faktoriyeli: " + getFact(i));
+        }
+
+    }
+
+    static int getFact(int num) {
+        if (num == 0)
+            return 1;
+        return num * getFact(num - 1);
+    }
 }
