49e4ed5a3475019d5ff0a3af7dfff9eae70e8c67
Özgür Tokay <ozytky@gmail.com>
2024-10-25 12:21:01 +0300

    day19

--- october2024/Day19.java
+++ october2024/Day19.java
@@ -2,5 +2,17 @@ package october2024;
 
 // girilen sayının mükemmel olup olmadığını bulan program
 public class Day19 {
-    
+    public static void main(String[] args) {
+        int num = 6;
+        int total = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                total += i;
+            }
+        }
+        if (total == num) {
+            System.out.println("Mükemmel Sayıdır");
+        } else
+            System.out.println("Mükemmel Sayı Değildir");
+    }
 }
