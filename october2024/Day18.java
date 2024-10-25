e1524f6cb43d0948992f3f411e37d428ea5790d4
Özgür Tokay <ozytky@gmail.com>
2024-10-25 12:20:56 +0300

    Day18

--- october2024/Day18.java
+++ october2024/Day18.java
@@ -2,5 +2,17 @@ package october2024;
 
 // bir sayının asal olup olmadığını bulan program
 public class Day18 {
-    
+    public static void main(String[] args) {
+        int num = 14;
+
+        String result = "Asaldır";
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                result = "Asal değildir";
+                break;
+            }
+        }
+        System.out.println(result);
+
+    }
 }
