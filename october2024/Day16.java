288a6085f70a8b4c24e0fe2b4fa292446c39f538
Özgür Tokay <ozytky@gmail.com>
2024-10-25 11:39:35 +0300

    day16

--- october2024/Day16.java
+++ october2024/Day16.java
@@ -2,5 +2,8 @@ package october2024;
 
 // girilen iki sayı arasındaki sayılardan 7 nin katı olanları bulan program
 public class Day16 {
-    
+    public static void main(String[] args) {
+        int smallNum = 3;
+        int bigNum ;
+    }
 }
