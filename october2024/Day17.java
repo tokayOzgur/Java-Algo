656432f60187d7bb84d6c8f64c977c713559cdb9
Özgür Tokay <ozytky@gmail.com>
2024-10-25 12:20:49 +0300

    Day17

--- october2024/Day17.java
+++ october2024/Day17.java
@@ -2,5 +2,14 @@ package october2024;
 
 // girilen sayının 0 mı, negatif mi veya pozitif mi olduğunu bulan program
 public class Day17 {
-    
+    public static void main(String[] args) {
+        int num = -10;
+
+        if (num > 0) {
+            System.out.println("Pozitif Sayı");
+        } else if (num < 0) {
+            System.out.println("Negatif Sayı");
+        } else
+            System.out.println("Sıfır");
+    }
 }
