e05c8c8c1f140c767b9b72345239f4b7476d6660
Özgür Tokay <ozytky@gmail.com>
2024-10-25 11:39:21 +0300

    day14

--- october2024/Day14.java
+++ october2024/Day14.java
@@ -2,5 +2,17 @@ package october2024;
 
 // klavyeden girilen n adet sayının toplamını ve aritmetik ort. bulan program
 public class Day14 {
-    
+    public static void main(String[] args) {
+        int n[] = { 2, 4, 5, 7, 8 };
+        int toplam = 0;
+        float aOrt = 0;
+
+        for (int i : n) {
+            toplam += i;
+        }
+        aOrt = toplam / n.length;
+
+        System.out.println("Toplam: " + toplam);
+        System.out.println("Aritmetik Ortalama: " + toplam / aOrt);
+    }
 }
