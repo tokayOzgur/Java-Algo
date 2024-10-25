5ba19395bc88fa545815cbee92629bc0016d230c
Özgür Tokay <ozytky@gmail.com>
2024-10-25 16:48:54 +0300

    day21

--- october2024/Day21.java
+++ october2024/Day21.java
@@ -1,5 +1,27 @@
 package october2024;
 
+// 100-999 arasında olan basamaklarının toplamı basamak sayısına eşit olan tamsayıları bulan program
 public class Day21 {
-    
+    public static void main(String[] args) {
+
+        for (int i = 100; i < 999; i++) {
+            if (calculate(i))
+                System.out.println(i);
+        }
+
+    }
+
+    static boolean calculate(int num) {
+        int number = num;
+        int i = number % 10;
+        number /= 10;
+        int j = number % 10;
+        number /= 10;
+        int k = number % 10;
+
+        if ((i + j + k) == 3)
+            return true;
+        return false;
+
+    }
 }
